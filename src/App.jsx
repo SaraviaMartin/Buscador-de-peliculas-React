d9236d42e6254279982dad67f44c6d0be0df834e
Juan <saraviamartin16@hotmail.com>
2023-07-26 15:24:29 -0300

    create hook useMovies, useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import responseMovies from './mocks/with-results.json'
-import withoutResults from './mocks/no-results.json'
+import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movies'
+
+
 
 function App() {
-  const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
+  const {movies: mappedMovies} = useMovies()
 
   return (
     <div className='page'>
@@ -19,25 +20,7 @@ function App() {
       </header>
 
     <main>
-      {
-        hasMovies
-          ? (
-            <ul>
-              {
-                movies.map(movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title} />
-                  </li>
-                ))
-              }
-            </ul>
-          )
-          :(
-            <p>No se encontraron peliculas para esta busqueda</p>
-          )
-      }
+      <Movies movies={mappedMovies}/>
     </main>
       
     </div>

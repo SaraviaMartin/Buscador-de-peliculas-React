0f28eceb437c864efc9311f3e529e7fb11027ec1
Juan <saraviamartin16@hotmail.com>
2023-08-03 14:59:08 -0300

    evitar la misma busqueda dos veces

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,15 +1,18 @@
 import { searchMovies } from '../services/movies'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export function useMovies ({search}){
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
+    const previusSearch = useRef(search)
 
     const getMovies = async () => {
+      if(search === previusSearch.current) return 
       try {
         setLoading(true)
         setError(null)
+        previusSearch.current = search
         const newMovies = await searchMovies({search})
         setMovies(newMovies)
       }catch(e){
